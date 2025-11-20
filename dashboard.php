15169b895d954b3157a0a0d38a2f604fbdc991aa
tayo123-byte <tiolubis4@gmail.com>
2025-11-20 20:54:14 +0700

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,25 @@
-ECHO is on.
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>Selamat Datang, <?php echo $_SESSION['username']; ?>!</h2>
+    <p>Role: <?php echo $_SESSION['role']; ?></p>
+
+    <a href="logout.php" class="logout">Logout</a>
+</div>
+
+</body>
+</html>
